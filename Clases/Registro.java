61bcf9a956fc7f878750132dd19af2ac200f5ebf
Julian Zapata Vidal <julianzapatavidal@gmail.com>
2023-09-01 16:02:28 -0300

    (nuevo) Se usa un archivo de texto para guardar toda la información, se lee, actualiza, borra, y busca a partir del mismo. (modificación) Se trasladan los subprocesos de Principal a Registro para adecuarlo a lo anterior. (otros) cambios menores

--- Clases/Registro.java
+++ Clases/Registro.java
@@ -2,41 +2,114 @@ package Clases;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.Scanner;
 
 import Clases.Alimentos;
 
 
 public class Registro {
 
-    public static void Escritura(String Fnombre, double FHC, double FPr, double FL){
-        try{
-            BufferedWriter bw = new BufferedWriter(new FileWriter("registro.txt"));
-
-                bw.write("\n{" + Fnombre + "\n" + FHC + "\n" + FPr + "\n" + FL + "}");
-                bw.close();
-
-        } catch (FileNotFoundException fnfe){System.out.println("Archivos no encontrados");}
-        catch (IOException ioe){System.out.println("Error en la lectura o escritura");}
-    }
-
-    public static void Lectura(){
-        try{
-            BufferedReader br = new BufferedReader(new FileReader("lista.txt"));
-            String linea = "";
-
-            while (linea != null ){
-                linea = br.readLine();
-            }
-            br.close();
-            //Los bloques catch capturan las excepciones y ejecutan una secuencia de codigo dependiendo de la excepcion encontrada
-        } catch (FileNotFoundException noEncontrado){
-            System.out.println("No se encuentra el archivo lista.txt");
-        } catch (IOException corrupto){
-            System.out.println("No se puede leer el archivo lista.txt");
+    public static void guardar(String Fnombre, double FHC, double FPr, double FL) throws IOException {
+        File file = new File("registro.txt");
+        if (!file.exists()) {
+            file.createNewFile();
         }
 
+        try (FileWriter writer = new FileWriter(file, true);
+             BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+            bufferedWriter.write(Fnombre + "," + FHC + "," + FPr + "," + FL + "\n");
+        }
+    }
+
+    /**
+     * Lectura busca un nombre de alimento dentro del archivo y devuelve un objeto Alimentos
+     * @param nombre nombre del alimetno buscado
+     * @return objeto Alimentos
+     * @throws IOException
+     */
+    public static Alimentos lectura(String nombre) throws IOException {
+        Alimentos alimento = null;
+        boolean Encontrado = false;
+
+        File file = new File("registro.txt");
+        if (!file.exists()) {
+            return null;
+        }
+
+        try (FileReader reader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(reader)) {
+            String linea;
+            while ((linea = bufferedReader.readLine()) != null) {
+                String[] datos = linea.split(",");
+
+                if (datos[0].equals(nombre)) {
+                    alimento = new Alimentos();
+                    alimento.setNombreAlimento(datos[0]);
+                    alimento.setHidratos(Double.parseDouble(datos[1]));
+                    alimento.setProteina(Double.parseDouble(datos[2]));
+                    alimento.setLipidos(Double.parseDouble(datos[3]));
+                    Encontrado = true;
+                    break;
+                }
+            }
+
+            if (!Encontrado){
+                System.out.println("No se encontró");
+            }
+        }
+
+        return alimento;
+    }
+
+    public static void borrar(String nombre) throws IOException {
+        File file = new File("registro.txt");
+        File tempFile = new File("registro.tmp");
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String linea;
+        while ((linea = reader.readLine()) != null) {
+            if (!linea.equals(nombre)) {
+                writer.write(linea + "\n");
+            }
+        }
+
+        reader.close();
+        writer.close();
+
+        file.delete();
+        tempFile.renameTo(file);
+    }
+
+    public static void actualizar() {
+        Scanner sc = new Scanner(System.in);
+        Alimentos aux = new Alimentos();
+        String NombreNuevo;
+        double HC, Pr, L;
+
+        System.out.println("¿Qué alimento quisiera actualizar?");
+        Alimentos viejo = lectura(sc.nextLine());
+        //Esto debería devolver el objeto correspondiente al alimento que se quiere actualizar
+
+        //Guardo todos los nuevos datos en aux,
+        System.out.println("Ingrese el nuevo nombre del alimento");
+        NombreNuevo = sc.nextLine();
+        System.out.printf("Hidratos de carbono: %f \n Ingrese nuevo valor:", viejo.getHidratos());
+        HC = Double.parseDouble(sc.next());
+        System.out.printf("Proteínas: %f \n Ingrese nuevo valor:", viejo.getProteinas());
+        Pr = Double.parseDouble(sc.next());
+        System.out.printf("Lípidos: %f \n Ingrese nuevo valor:", viejo.getLipidos());
+        L = Double.parseDouble(sc.next());
+
+        //Borramos el viejo
+        borrar(viejo.getNombreAlimento());
+        //Sumamos el nuevo
+        guardar(NombreNuevo, HC, Pr, L);
+    }
 }
--- Principal.java
+++ Principal.java
@@ -1,23 +1,18 @@
 import Clases.Alimentos;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main (String[] args){
+    public static void main(String[] args) throws IOException {
 
-        //Inicializamos todo lo necesario
         Alimentos comida = new Alimentos();
         Scanner sc = new Scanner(System.in);
-        //boolean flag=false; //Dummie para opciones
         int opSwitch;
-        ArrayList<Alimentos> listaAlimentos = new ArrayList<>();
+        //ArrayList<Alimentos> listaAlimentos = new ArrayList<>();
 
-        //boolean flagNoEncontrado2 = true; // Para 2.a.
-        //boolean flagNoEncontrado3 = true; // Para 3.
-        //boolean flagNoEncontrado4 = true; // Para 4.
-
-
+        // * Machete:
         // * 1. Crear alimento
         // * 1.a. Guardar en base de datos
         // * 1.b. ¿Crear varias bases de datos?
@@ -28,151 +23,112 @@ public class Principal {
         // * 4. Borrar alimento individual
         // * 5. Cálculo de nutrientes
 
-        do{
+        do {
             Clases.Menues.MenuPrincipal();
             System.out.print("Ingrese la opción deseada \t");
             opSwitch = sc.nextInt();
 
             switch (opSwitch) {
-                case 1 ->
-                    /* 1. Crear alimento */
-                    /* 1.a Guardar en base de datos */
-                        listaAlimentos.add(Nuevo());
+                case 1 -> listaAlimentos.add(Nuevo());
                 case 2 -> {
-                    /* 2. Leer alimento */
                     System.out.println("¿Desea revisar... \n1. un alimento en particular? \t2. todos los alimentos?");
                     switch (sc.nextInt()) {
-                        case 1 -> { //2.a. Búsqueda individual
+                        case 1 -> {
                             System.out.println("Ingrese el nombre del alimento a buscar");
                             String buscado = sc.next();
-                            int posicion = Busqueda(buscado, listaAlimentos);
                             System.out.printf("\n " +
                                             "Nombre: %s \n --------------- \n " +
                                             "Hidratos: %f \t Proteínas: %f \t Lípidos: %f \n " +
                                             "--------------- ",
-                                    listaAlimentos.get(posicion).getNombreAlimento(),
-                                    listaAlimentos.get(posicion).getHidratos(),
-                                    listaAlimentos.get(posicion).getProteinas(),
-                                    listaAlimentos.get(posicion).getLipidos());
+                                    Clases.Registro.lectura(buscado).getNombreAlimento(),
+                                    Clases.Registro.lectura(buscado).getHidratos(),
+                                    Clases.Registro.lectura(buscado).getProteinas(),
+                                    Clases.Registro.lectura(buscado).getLipidos());
                         }
-                        case 2 -> { //2.b. Búsqueda general
+                        case 2 -> {
                             System.out.println("Lista de alimentos registrados");
-                            for (Alimentos busqueda : listaAlimentos) {
-                                System.out.printf("\n Nombre: %s \n --------------- \n Hidratos: %f \t Proteínas: %f \t Lípidos: %f \n --------------- ", busqueda.getNombreAlimento(), busqueda.getHidratos(), busqueda.getProteinas(), busqueda.getLipidos());
+                            for (Alimentos aux : listaAlimentos) {
+                                System.out.printf("\n Nombre: %s \n --------------- \n Hidratos: %f \t Proteínas: %f \t Lípidos: %f \n --------------- ", aux.getNombreAlimento(), aux.getHidratos(), aux.getProteinas(), aux.getLipidos());
                             }
                         }
-                        default -> //Opción incorrecta
-                                System.out.println("Opción incorrecta");
+                        default -> System.out.println("Opción incorrecta");
                     }
                 }
                 case 3 -> {
-                    /* 3. Actualizar alimento individual */
                     System.out.println("Ingrese el nombre del alimento a actualizar");
                     String buscado = sc.next();
-                    int posicion = Busqueda(buscado, listaAlimentos);
-                    listaAlimentos.add(Actualizado(listaAlimentos.get(posicion), listaAlimentos, posicion));
+                    listaAlimentos.add(Actualizado(Clases.Registro.lectura(buscado), listaAlimentos));
                 }
                 case 4 -> {
-                    /* 4. Borrar alimento individual */
                     System.out.println("Ingrese el nombre del alimento que quiere eliminar");
                     String buscado = sc.next();
-                    int posicion = Busqueda(buscado, listaAlimentos);
-                    listaAlimentos.remove(posicion);
+                    listaAlimentos.remove(Clases.Registro.lectura(buscado));
                 }
-                case 5 ->
-                    /* 5. Cálculo de nutrientes */
-                        Calculo(listaAlimentos);
+                case 5 -> Calculo(listaAlimentos);
                 default -> {
                 }
             }
-        } while (opSwitch<6);          
+        } while (opSwitch < 6);
     } //Fin Main
 
     //Subprocesos
 
-    public static Alimentos Nuevo(){
-        Scanner sc = new Scanner (System.in);
+    public static Alimentos Nuevo() throws IOException {
+        Scanner sc = new Scanner(System.in);
         String Fnombre;
-        double FHC,FPr,FL;
+        double FHC, FPr, FL;
         System.out.println("Ingrese el nombre del alimento");
-            Fnombre=sc.next();
-            System.out.println("Ingrese los gramos de hidratos de carbono cada 100 gramos");
-            FHC=Double.parseDouble(sc.next());
-            System.out.println("Ingrese los gramos de proteína cada 100 gramos");
-            FPr=Double.parseDouble(sc.next());
-            System.out.println("Ingrese los gramos de lípidos cada 100 gramos");
-            FL=Double.parseDouble(sc.next());
-        return new Alimentos(Fnombre,FHC,FPr,FL);
+        Fnombre = sc.next();
+        System.out.println("Ingrese los gramos de hidratos de carbono cada 100 gramos");
+        FHC = Double.parseDouble(sc.next());
+        System.out.println("Ingrese los gramos de proteína cada 100 gramos");
+        FPr = Double.parseDouble(sc.next());
+        System.out.println("Ingrese los gramos de lípidos cada 100 gramos");
+        FL = Double.parseDouble(sc.next());
+        Clases.Registro.guardar(Fnombre, FHC, FPr, FL);
+        return new Alimentos(Fnombre, FHC, FPr, FL);
     }
-    
-    public static Alimentos Actualizado(Alimentos Fcomida,ArrayList<Alimentos> FlistaAlimentos, int Fposicion){
-        Scanner sc = new Scanner (System.in);
+
+    public static Alimentos Actualizado(Alimentos Fcomida, ArrayList<Alimentos> FlistaAlimentos) {
+        Scanner sc = new Scanner(System.in);
         String Fnombre;
-        double FHC,FPr,FL;
+        double FHC, FPr, FL;
         System.out.println("Ingrese el nuevo nombre del alimento");
-        Fnombre=sc.next();
-            System.out.printf("Hidratos de carbono: %f \n Ingrese nuevo valor:", Fcomida.getHidratos());
-        FHC=Double.parseDouble(sc.next());
-            System.out.printf("Proteínas: %f \n Ingrese nuevo valor:", Fcomida.getProteinas());
-        FPr=Double.parseDouble(sc.next());
-            System.out.printf("Lípidos: %f \n Ingrese nuevo valor:", Fcomida.getLipidos());
-        FL=Double.parseDouble(sc.next());
+        Fnombre = sc.next();
+        System.out.printf("Hidratos de carbono: %f \n Ingrese nuevo valor:", Fcomida.getHidratos());
+        FHC = Double.parseDouble(sc.next());
+        System.out.printf("Proteínas: %f \n Ingrese nuevo valor:", Fcomida.getProteinas());
+        FPr = Double.parseDouble(sc.next());
+        System.out.printf("Lípidos: %f \n Ingrese nuevo valor:", Fcomida.getLipidos());
+        FL = Double.parseDouble(sc.next());
         FlistaAlimentos.remove(Fposicion);
-        Fcomida = new Alimentos(Fnombre,FHC,FPr,FL);
+        Fcomida = new Alimentos(Fnombre, FHC, FPr, FL);
         return Fcomida;
     }
-    
-    public static void Calculo(ArrayList<Alimentos> FlistaAlimentos){
+
+    public static void Calculo(ArrayList<Alimentos> FlistaAlimentos) throws IOException {
         Scanner sc = new Scanner(System.in);
-        boolean flagNoEncontradoCalculo = true;
         boolean flagSalidaDoCalculo = true;
 
         //Acumuladores
-        double acum_hidrato=0, acum_proteina=0, acum_lipido=0;
+        double acum_hidrato = 0, acum_proteina = 0, acum_lipido = 0;
 
         do {
             System.out.println("Ingrese el nombre del alimento");
-            String Fcomida=sc.next();
+            String buscado = sc.next();
+            Alimentos aux = Clases.Registro.lectura(buscado);
 
-            for (Alimentos alimentos : FlistaAlimentos) {
-                String auxBusqueda = alimentos.getNombreAlimento();
-                if (auxBusqueda.equals(Fcomida)){
-                    int posicion=FlistaAlimentos.indexOf(alimentos);
-                    acum_hidrato+=FlistaAlimentos.get(posicion).getHidratos();
-                    acum_proteina+=FlistaAlimentos.get(posicion).getProteinas();
-                    acum_lipido+=FlistaAlimentos.get(posicion).getLipidos();
-                    flagNoEncontradoCalculo = false;
-                }
-            }
+            acum_hidrato += aux.getHidratos();
+            acum_proteina += aux.getProteinas();
+            acum_lipido += aux.getLipidos();
 
-            if (flagNoEncontradoCalculo) {System.out.println("Alimento no encontrado");}
+            System.out.printf("Totales \nHidratos: %f \tProteínas: %f \tLipidos: %f \n", acum_hidrato, acum_proteina, acum_lipido);
 
             System.out.println("¿Desea ingresar otro alimento?");
             if (sc.next().equals("no")) {
                 flagSalidaDoCalculo = false; // ¿¿¿
             }
-
-
-
         } while (flagSalidaDoCalculo);
 
-    }
-    public static int Busqueda(String buscado, ArrayList<Alimentos> FlistaAlimentos){
-        boolean NoEncontrado = true;
-        int resultado = 0;
-
-        for (Alimentos elemento : FlistaAlimentos) {
-            String auxBusqueda = elemento.getNombreAlimento();
-            if (auxBusqueda.equals(buscado)) {
-                resultado = FlistaAlimentos.indexOf(elemento);
-                NoEncontrado = false;
-            }
-        }
-        if (NoEncontrado) {
-            System.out.println("Búsqueda sin resultados");
-            resultado = -1;
-        }
-
-        return resultado;
     }
 }
